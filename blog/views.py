1fee275873a85b1734d47aa1943c72111de20690
rootsec0ps <promotie@tutamail.com>
2025-08-23 16:46:44 +0200

    added reply function and changed some of the layout

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 
-from .models import Post
+from .models import Post, Reply
+from .forms import ReplyForm
 
 # Create your views here.
 class PostListView(ListView):
     model = Post
     template_name = "post_list.html"
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = "post_detail.html"
+#class PostDetailView(DetailView):
+#    model = Post
+#    template_name = "post_detail.html"
+
+def post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    replies = post.reply_set.order_by('-date_added')
+    context = {'post': post, 'replies': replies}
+    return render(request, 'post_detail.html', context)
+
+def new_reply(request, post_id):
+    post = Post.objects.get(id=post_id)
+    replies = post.reply_set.order_by('-date_added')
+
+    if request.method != 'POST':
+        # When no data is submitted, create a blank form
+        form = ReplyForm()
+
+    else:
+        # POST data submitted, process data
+        form = ReplyForm(data=request.POST)
+        if form.is_valid():
+            new_reply = form.save(commit=False)
+            new_reply.post = post
+            new_reply.save()
+            return redirect('blog:post_detail', post_id=post_id)
+    
+    # Display a blank or invalid form
+    context = {'form': form, 'post': post, 'replies': replies}
+    return render(request, 'new_reply.html', context)
