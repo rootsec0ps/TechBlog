78808473072ec63641a1a4b40dcf30efec35faf0
rootsec0ps <promotie@tutamail.com>
2025-08-23 22:09:57 +0200

    some tweaks to the build script

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+echo "Building project packages..."
+python3 -m pip install -r requirements.txt
+
+echo "Migrating database..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
