3f818ec449cd2b24b02ddcf2b89258dd3dc74c0f
rootsec0ps <promotie@tutamail.com>
2025-09-07 18:37:17 +0200

    changed settings for supabase

--- tech_blog/settings.py
+++ tech_blog/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
 #    }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg3',
-        'NAME': os.environ.get['DB_NAME'],
-        'USER': os.environ.get['DB_USER'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': os.environ['DB_HOST'],
         'PORT': os.environ['DB_PORT'],
