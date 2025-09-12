2e3f133c479e9d13f07c643fee9706f4d57b346a
rootsec0ps <promotie@tutamail.com>
2025-09-12 14:09:04 +0200

    changed the settings to be able to serve css

--- tech_blog/settings.py
+++ tech_blog/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 
 # Added code by me to see if this will work on Vercel for loading static files
 #STATICFILES_DIRS = [BASE_DIR/'blog'/'static',]
